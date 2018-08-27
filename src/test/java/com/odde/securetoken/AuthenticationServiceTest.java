b38e3801fa7d41cceb37ddfc1584b379160f389e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-08-27 13:08:20 +0800

    :recycle: refactoring test by using mockito

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -3,38 +3,25 @@ package com.odde.securetoken;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
     @Test
-    public void is_valid_test() {
-        StubProfile stubProfile = new StubProfile();
-        StubToken stubToken = new StubToken();
+    public void is_valid_test_mockito() {
+        Profile stubProfile = mock(Profile.class);
+        when(stubProfile.getPassword("joey")).thenReturn("91");
+
+        Token stubToken = mock(Token.class);
+        when(stubToken.getRandom(anyString())).thenReturn("000000");
+
         AuthenticationService target = new AuthenticationService(stubProfile, stubToken);
 
         boolean actual = target.isValid("joey", "91000000");
 
         assertTrue(actual);
-    }
-
-    class StubProfile implements Profile {
-
-        @Override
-        public String getPassword(String account) {
-            if (account.equals("joey")) {
-                return "91";
-            } else {
-                throw new UnsupportedOperationException();
-            }
-        }
-    }
-
-    class StubToken implements Token {
-
-        @Override
-        public String getRandom(String account) {
-            return "000000";
-        }
     }
 
 }
