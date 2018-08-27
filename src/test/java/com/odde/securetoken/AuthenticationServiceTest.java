af00172d133402b4cc1e00b5457f23b034d7e605
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-08-27 13:20:41 +0800

    :recycle: add an invalid test

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -2,6 +2,7 @@ package com.odde.securetoken;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -9,19 +10,40 @@ import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
+    Profile stubProfile = mock(Profile.class);
+    Token stubToken = mock(Token.class);
+    AuthenticationService target = new AuthenticationService(stubProfile, stubToken);
+
     @Test
-    public void is_valid_test_mockito() {
-        Profile stubProfile = mock(Profile.class);
-        when(stubProfile.getPassword("joey")).thenReturn("91");
+    public void is_valid() {
+        givenPassword("joey", "91");
+        givenToken("000000");
 
-        Token stubToken = mock(Token.class);
-        when(stubToken.getRandom(anyString())).thenReturn("000000");
+        shouldBeValid("joey", "91000000");
+    }
 
-        AuthenticationService target = new AuthenticationService(stubProfile, stubToken);
+    @Test
+    public void is_invalid() {
+        givenPassword("joey", "91");
+        givenToken("000000");
 
-        boolean actual = target.isValid("joey", "91000000");
+        shouldBeInvalid("joey", "wrong pass code");
+    }
 
-        assertTrue(actual);
+    private void shouldBeInvalid(String account, String passCode) {
+        assertFalse(target.isValid(account, passCode));
+    }
+
+    private void shouldBeValid(String account, String passCode) {
+        assertTrue(target.isValid(account, passCode));
+    }
+
+    private void givenToken(String token) {
+        when(stubToken.getRandom(anyString())).thenReturn(token);
+    }
+
+    private void givenPassword(String account, String password) {
+        when(stubProfile.getPassword(account)).thenReturn(password);
     }
 
 }
