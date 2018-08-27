eec9c67b0234273b7c7bb9a50af7b804348877af
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-08-27 12:43:56 +0800

    :sparkles: add test

---
+++ src/main/java/com/odde/securetoken/Token.java
@@ -0,0 +1,5 @@
+package com.odde.securetoken;
+
+public interface Token {
+    String getRandom(String account);
+}
--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -8,11 +8,33 @@ public class AuthenticationServiceTest {
 
     @Test
     public void is_valid_test() {
-        AuthenticationService target = new AuthenticationService();
+        StubProfile stubProfile = new StubProfile();
+        StubToken stubToken = new StubToken();
+        AuthenticationService target = new AuthenticationService(stubProfile, stubToken);
 
         boolean actual = target.isValid("joey", "91000000");
 
         assertTrue(actual);
+    }
+
+    class StubProfile implements Profile {
+
+        @Override
+        public String getPassword(String account) {
+            if (account.equals("joey")) {
+                return "91";
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        }
+    }
+
+    class StubToken implements Token {
+
+        @Override
+        public String getRandom(String account) {
+            return "000000";
+        }
     }
 
 }
